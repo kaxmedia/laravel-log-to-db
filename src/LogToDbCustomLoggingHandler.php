9303b90d10e5ca6a7da1cdd2c6a5f8672c2ee860
Laszlo Kruchio <laszlo.kruchio@kaxmedia.com>
2019-10-02 09:53:30 +0100

    Formated the date.

--- src/LogToDbCustomLoggingHandler.php
+++ src/LogToDbCustomLoggingHandler.php
@@ -112,7 +112,7 @@ class LogToDbCustomLoggingHandler extends AbstractProcessingHandler
                     $this->saveWithQueueName,
                     $this->saveWithQueueConnection);
                 $log->newFromMonolog($record);
-                PublishLogUpdated::dispatch($record['created_at'], $record['level_name'], $record['message']);
+                PublishLogUpdated::dispatch($record['context']['publish'], date('Y-m-d H:i:s', strtotime($record['datetime'])), $record['level_name'], $record['message']);
             } catch (\Exception $e) {
 
             }
